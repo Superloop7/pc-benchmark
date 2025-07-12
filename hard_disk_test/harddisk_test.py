17ed89fa683c2535c1aefd70f75f66999c3dc84e
Mark Geng <gengzhaoxuan@gmail.com>
2025-07-12 21:48:18 +0930

    remove flush incase keep read from memory:

--- hard_disk_test/harddisk_test.py
+++ hard_disk_test/harddisk_test.py
@@ -2,6 +2,7 @@ import os, time
 from dataclasses import dataclass
 
 # @dataclass can replace __init__
+@dataclass
 class HardDiskResult:
     mode: str
     # the speed unit is MB/s
@@ -9,19 +10,25 @@ class HardDiskResult:
     read_speed: float
     # size_unit is MB
     size_tested: int
-    wirte_time_used: float
-    read_time: float
+    write_time_used: float
+    read_time_used: float
 
     # it will more easy to see all data
     def __repr__(self):
-        return (
-            f"SsdTestResult("
-            f"write_speed={self.write_speed:.2f}, "
-            f"read_speed={self.read_speed:.2f}, "
-            f"size_tested={self.size_tested}, "
-            f"write_time={self.write_time:.2f}, "
-            f"read_time={self.read_time:.2f})"
-        )
+        if self.mode == "write_test":
+            return (
+                f"SsdTestResult("
+                f"write_speed={self.write_speed:.2f}, "
+                f"size_tested={self.size_tested}, "
+                f"write_time={self.write_time_used:.2f}"
+            )
+        elif self.mode == "read_test":
+            return (
+                f"SsdTestResult("
+                f"read_speed={self.read_speed:.2f}, "
+                f"size_tested={self.size_tested}, "
+                f"read_time={self.read_time_used:.2f})"
+            )
 
     def __str__(self):
         if self.mode == "write_test":
@@ -30,14 +37,14 @@ class HardDiskResult:
             return f"[HARDDISK RESULT] Read  Speed : {self.read_speed:.2f} MB/s"
     
 class HardDiskTest:
-    def __init__(self, file_path="harddisk_test_temp.bin", duration=1, block_size=256 * 1024):
+    def __init__(self, file_path="write_test.bin", duration=1, block_size=256 * 1024):
         self.file_path = file_path
         self.duration = duration
         self.block_size = block_size
         # one binary 0 is 1 byte each round 256KB
         self.data_block = b"0" * block_size
 
-    def harddisk_wirte_test(self) -> HardDiskResult:
+    def harddisk_write_test(self) -> HardDiskResult:
         # write test
         total_written_size = 0
         write_start = time.perf_counter()
@@ -48,10 +55,6 @@ class HardDiskTest:
             while True:
                 file.write(self.data_block)
                 total_written_size += 1
-                # write data to memory
-                file.flush()
-                # synchronize to harddisk
-                os.fsync(file.fileno())
                 # protect my disk
                 if time.perf_counter() - write_start >= self.duration:
                     break
@@ -63,15 +66,18 @@ class HardDiskTest:
         write_speed = self.written_size / write_time
         print(f"[INFO] Write Test finished")
         return HardDiskResult(
-            write_speed=write_speed,
-            size_tested= self.written_size,
-            write_time=write_time,
-            mode = "write_test"
+            write_speed = write_speed,
+            write_time_used = write_time,
+            size_tested = self.written_size,
+            mode = "write_test",
+            read_speed = 0,
+            read_time_used = 0
         )
     
     def harddisk_read_test(self) -> HardDiskResult:
         # Read test
         print(f"[INFO] Starting HardDisk write test ...")
+
         read_start = time.perf_counter()
         with open(self.file_path, "rb") as file:
             # Read the file in blocks to minimize memory usage
@@ -86,16 +92,20 @@ class HardDiskTest:
             os.remove(self.file_path)
         except Exception as e:
             print(f"[WARN] Failed to remove the temp file: {e}")
+
         print(f"[INFO] Read Test finished")
-        return HardDiskTest(
-            read_speed=read_speed,
-            read_time=read_time,
-            mode = "read_test"
+        return HardDiskResult(
+            read_speed = read_speed,
+            read_time_used = read_time,
+            size_tested = self.written_size,
+            mode = "read_test",
+            write_speed = 0,
+            write_time_used = 0
         )
     
 if __name__ == "__main__":
     tester = HardDiskTest()
-    write_result = tester.harddisk_wirte_test()
+    write_result = tester.harddisk_write_test()
     print(write_result)
     read_result = tester.harddisk_read_test()
-    print(write_result)
+    print(read_result)
--- run_all_test.py
+++ run_all_test.py
@@ -1,10 +1,10 @@
 import cpu_test.cpu_test as cpu_test
-import hard_disk_test.ssd_test as ssd_test
+import hard_disk_test.harddisk_test as harddisk_test
 import webbrowser
 
 
 cpu_result = cpu_test.cpu_test()
-ssd_result = ssd_test.ssd_test()
+ssd_result = harddisk_test.ssd_test()
 
 with open("myfile.html", "r", encoding="utf-8") as f:
     html = f.read()
