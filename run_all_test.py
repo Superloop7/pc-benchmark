66005c677a989ab528eaf7e86e461d4a9fb3431c
Mark Geng <gengzhaoxuan@gmail.com>
2025-07-12 22:50:17 +0930

    finfish all program

--- run_all_test.py
+++ run_all_test.py
@@ -6,25 +6,25 @@ if __name__ == "__main__":
     cpu_tester = cpu_test.CpuTest(calculate_time=20)
     single_core_result =cpu_tester.run_single()
     multi_cores_result =cpu_tester.run_multi()
-    single_core_result = str(single_core_result).split(" ")[1:]
-    multi_cores_result = str(multi_cores_result).split(" ")[1:]
-    cpu_result = f"{single_core_result}\n {multi_cores_result}"
-
+    cpu_result = (
+        f"Single-core score: {single_core_result.scores:.2f} Points<br>"
+        f"Multi-core score ({multi_cores_result.cpu_count} cores): {multi_cores_result.scores:.2f} Points"
+    )
     harddisk_tester = harddisk_test.HardDiskTest()
     write_result = harddisk_tester.harddisk_write_test()
     read_result = harddisk_tester.harddisk_read_test()
-    write_result = str(write_result).split(" ")[1:]
-    read_result = str(read_result).split(" ")[1:]
-    harddisk_result = f"{write_result}\n {read_result}"
-
-    with open("myfile.html", "r", encoding="utf-8") as f:
+    harddisk_result = (
+        f"Write Speed: {write_result.write_speed:.2f} MB/s<br>"
+        f"Read Speed: {read_result.read_speed:.2f} MB/s"
+    )
+    with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
 
     result_script = f"<p>{cpu_result}</p>\n<p>{harddisk_result}</p>\n"
 
     final_report = html.replace("<result>", result_script)
 
-    with open("result.html", "w", encoding="utf-8") as f:
+    with open("pc_bench_result.html", "w", encoding="utf-8") as f:
         f.write(final_report)
 
-    webbrowser.open("result.html")
+    webbrowser.open("pc_bench_result.html")
